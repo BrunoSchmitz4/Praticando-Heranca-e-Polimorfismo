9eb5ff184af1b48d1d98aaef9b72ee90bc84f17d
Bruno Schmitz da Silva <brunosch250@gmail.com>
2023-06-28 14:03:03 -0300

    Initial commit

---
+++ Entities/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecQuintApp.Entities
+{
+    // Esta é a superclasse
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+
+        }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        // O método Withdraw agora pode ser sobrescrito
+        // nas subclasses, o que permite isso, é o virtual.
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0 ;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecQuintApp.Entities
+{
+    // Esta é a subclasse
+    // para dizermos que ela herda de uma superclasse, colocamos o : SuperClasse
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        // Para reusarmos os construtores, podemos colocar o
+        // ": base(variaveis locais do construtor da superclasse)"
+        // E dentro, só chamará as props da subclasse
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+        
+    }
+}
